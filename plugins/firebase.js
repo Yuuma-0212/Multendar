57ebfe822f4ed859b9dfb96c16dd699103fd7248
yuuma-0212 <yuumaskate_yk@yahoo.co.jp>
2022-11-26 02:42:58 +0900

    dotenvの設定テスト

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,7 +2,25 @@ import { initializeApp, getApp } from "firebase/app"
 import { getFunctions, connectFunctionsEmulator, httpsCallable } from 'firebase/functions'
 import { getAuth, connectAuthEmulator } from "firebase/auth";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import axios from "axios";
 
+const getEnv = async () => {
+  await axios.get("https://weather-scheduler-test.azurewebsites.net/api/getFirebaseEnv").then((res) => {
+    firebaseConfig = {
+      apiKey: res.data.API_KEY,
+      authDomain: res.data.AUTH_DOMAIN,
+      projectId: res.data.PROJECT_ID,
+      storageBucket: res.data.STORAGE_BUCKET,
+      messagingSenderId: res.data.MESSAGING_SENDER_ID,
+      appId: res.data.APP_ID,
+      measurementId: res.data.MEASUREMENT_ID
+    }
+  });
+}
+
+let firebaseConfig = {};
+getEnv();
+/*
 export const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
@@ -11,7 +29,7 @@ export const firebaseConfig = {
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID,
   measurementId: process.env.MEASUREMENT_ID
-}
+}*/
 
 console.log("firebase config", firebaseConfig);
 
