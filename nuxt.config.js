f4fcf83ffc41fd6f5d800e8251e057b1379b0a23
yuuma-0212 <yuumaskate_yk@yahoo.co.jp>
2022-11-29 18:22:11 +0900

    dotenvの設定テスト

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,6 +66,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: 'https://weather-scheduler-test.azurewebsites.net/api',
+    https: true
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
