7ed137dd651b6ba24ef2f4d29fdd8eca66da2a38
yuuma-0212 <yuumaskate_yk@yahoo.co.jp>
2022-12-15 19:35:58 +0900

    swの設定テスト

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -4,9 +4,9 @@ import { getAuth, connectAuthEmulator } from "firebase/auth";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 import axios from "axios";
 
-export const getFirebaseApi = async () => {
-  const firebaseConfig = await axios.get("https://weather-scheduler-test.azurewebsites.net/api/getFirebaseEnv").then((res) => {
-    return {
+export const firebase = async () => {
+  await axios.get("https://weather-scheduler-test.azurewebsites.net/api/getFirebaseEnv").then((res) => {
+    const firebaseConfig = {
       apiKey: res.data.API_KEY,
       authDomain: res.data.AUTH_DOMAIN,
       projectId: res.data.PROJECT_ID,
@@ -15,16 +15,7 @@ export const getFirebaseApi = async () => {
       appId: res.data.APP_ID,
       measurementId: res.data.MEASUREMENT_ID
     }
-  });
 
-  console.log("firebaseConfig", firebaseConfig);
-  return firebaseConfig;
-}
-
-export const firebase = async () => {
-  await getFirebaseApi().then((res) => {
-    const firebaseConfig = res.data;
-    console.log("firebase", firebaseConfig);
     initializeApp(firebaseConfig);
   });
 }
