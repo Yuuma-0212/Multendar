9fe518516fa51f26850ca821aae18844835c6eef
yuuma-0212 <yuumaskate_yk@yahoo.co.jp>
2022-12-15 23:34:44 +0900

    swの設定テスト

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,4 +2,42 @@ importScripts("https://www.gstatic.com/firebasejs/9.11.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/9.11.0/firebase-messaging.js");
 importScripts("./static/sw-env.js");
 
-console.log("firebase-message-sw", swEnv);
+
+firebase.initializeApp({
+    messagingSenderId: swEnv.messagingSenderId
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background messages.
+const messaging = firebase.messaging();
+
+// 通知を受けとると push イベントが呼び出される。
+self.addEventListener(
+    "push",
+    function (event) {
+        let message = event.data.json();
+        console.log("event:push", message);
+        let messageTitle = message.notification.title;
+        let messageBody = message.notification.body;
+        let tag = "cuppa";
+
+        const notificationPromise = self.registration.showNotification(
+            messageTitle,
+            {
+                body: messageBody,
+                tag: tag,
+            }
+        );
+
+        event.waitUntil(notificationPromise);
+    },
+    false
+);
+
+// WEBアプリがバックグラウンドの場合にはsetBackGroundMessageHandlerが呼び出される。
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log("backgroundMessage");
+
+    return self.registration.showNotification(payload.title, {
+        body: payload.body,
+    });
+});
