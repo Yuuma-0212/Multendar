2f25c248ade7c3409fa4e118ac493b106bfcb376
yuuma-0212 <yuumaskate_yk@yahoo.co.jp>
2022-12-19 23:38:44 +0900

    swの設定テスト

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,12 +9,12 @@
 <script>
 import Header from "~/components/Header.vue";
 import Footer from "~/components/Footer.vue";
-import {getFcmToken} from "~/plugins/firebase-firestore.js";
+import { getFcmToken } from "~/plugins/firebase-firestore.js";
 
 export default {
   name: "DefaultLayout",
   components: { Header, Footer },
-  asyncData() {
+  async asyncData({ $axios }) {
     // fcmのswを登録する
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker
@@ -26,19 +26,26 @@ export default {
           console.log("Service worker registration failed, error:", error);
         });
     }
-  },
-  async mounted() {
+
     const title = "testTitle";
     const body = "hello fcm";
-    await this.$axios.$get("https://weather-scheduler-test.azurewebsites.net/api/getFirebaseEnv").then(async (res) => {
-      console.log("projectId", res.data);
-      const firebaseConfig = res.data.PROJECT_ID;
-      const fcmToken = await getFcmToken();
-      console.log("fcmToken", fcmToken);
-      console.log("fcmToken.data", fcmToken.data);
-      const fcmSendUrl = "https://fcm.googleapis.com//v1/projects/" + firebaseConfig.PROJECT_ID + "/messages:send";
-      //this.$axios.$post(fcmSendUrl)
-    })
-  }
+    await $axios
+      .get(
+        "https://weather-scheduler-test.azurewebsites.net/api/getFirebaseEnv"
+      )
+      .then(async (res) => {
+        console.log("projectId", res.data);
+        const firebaseConfig = res.data.PROJECT_ID;
+        const fcmToken = await getFcmToken();
+        console.log("fcmToken", fcmToken);
+        console.log("fcmToken.data", fcmToken.data);
+        const fcmSendUrl =
+          "https://fcm.googleapis.com//v1/projects/" +
+          firebaseConfig.PROJECT_ID +
+          "/messages:send";
+        //this.$axios.$post(fcmSendUrl)
+      });
+  },
+  async mounted() {},
 };
 </script>
