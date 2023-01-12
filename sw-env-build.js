9f3fb956aa017f57dd0b5e3eb8f078c80ad04025
yuuma-0212 <yuumaskate_yk@yahoo.co.jp>
2023-01-12 20:46:00 +0900

    envの設定変更テスト

--- sw-env-build.js
+++ sw-env-build.js
@@ -17,7 +17,7 @@ writeFileSync(path, data);
 
 const envPath = "./.env";
 const envData = `
-FIREBASE_API_KEY=${process.env.FIREBASE_API_KEY}',
+FIREBASE_API_KEY='${process.env.FIREBASE_API_KEY}',
 FIREBASE_AUTH_DOMAIN='${process.env.FIREBASE_AUTH_DOMAIN}',
 FIREBASE_PROJECT_ID='${process.env.FIREBASE_PROJECT_ID}',
 FIREBASE_STORAGE_BUCKET='${process.env.FIREBASE_STORAGE_BUCKET}',
