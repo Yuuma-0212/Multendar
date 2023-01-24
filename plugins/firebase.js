d224ad0b95796f6cecc2eb78c95df8fbd23870ae
yuuma-0212 <yuumaskate_yk@yahoo.co.jp>
2023-01-24 20:32:39 +0900

    プッシュ通知が動作しない不具合を修正

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,25 +3,6 @@ import { getFunctions, connectFunctionsEmulator, httpsCallable } from 'firebase/
 import { getAuth, connectAuthEmulator } from "firebase/auth";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
-/*
-export const firebase = async () => {
-  await axios.get("https://weather-scheduler-test.azurewebsites.net/api/getFirebaseEnv").then((res) => {
-    const firebaseConfig = {
-      apiKey: res.data.API_KEY,
-      authDomain: res.data.AUTH_DOMAIN,
-      projectId: res.data.PROJECT_ID,
-      storageBucket: res.data.STORAGE_BUCKET,
-      messagingSenderId: res.data.MESSAGING_SENDER_ID,
-      appId: res.data.APP_ID,
-      measurementId: res.data.MEASUREMENT_ID
-    }
-
-    initializeApp(firebaseConfig);
-  });
-}
-*/
-
-
 export const firebase = async () => {
   const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
@@ -37,6 +18,7 @@ export const firebase = async () => {
 
   initializeApp(firebaseConfig);
 
+  /*
   const auth = getAuth();
   const functions = getFunctions(getApp(), "asia-northeast1");
   const firestore = getFirestore();
@@ -44,4 +26,5 @@ export const firebase = async () => {
   connectAuthEmulator(auth, "http://localhost:9099");
   connectFunctionsEmulator(functions, 'localhost', 5001);
   connectFirestoreEmulator(firestore, 'localhost', 8080);
+  */
 }
