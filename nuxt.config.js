f5a0b88bf9693948de99c559021eb4054dcc954c
Yuuma-0212 <yuumaskate_yk@yahoo.co.jp>
2023-02-02 01:18:51 +0900

    アプリケーションエラーで表示されない不具合を修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,13 +75,16 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.BROWSER_BASE_URL,
+    //baseURL: process.env.BROWSER_BASE_URL,
+    baseURL: "http://localhost:3000"
   },
 
+  /*
   server: {
     host: '0.0.0.0',
     port: 3000
   },
+  */
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
