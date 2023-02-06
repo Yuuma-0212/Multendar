9bb4cabe5e2d3d5fac212da30c794363274ac7d4
Yuuma-0212 <yuumaskate_yk@yahoo.co.jp>
2023-02-07 01:42:05 +0900

    ログインしてからインストールして起動した場合トップ画面なのにログアウトになる不具合を一旦放置

--- components/Header.vue
+++ components/Header.vue
@@ -28,9 +28,6 @@ export default {
     isLogin: false
   }),
   created() {
-    // ブラウザからログイン中にインストールするとトップ画面なのにヘッダーがログアウトになるのを防ぐ
-    if (this.isLogin) this.isLogin = false;
-
     this.isLogin = this.$cookies.get("isLogin");
   },
   methods: {
