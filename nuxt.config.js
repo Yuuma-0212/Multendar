c9a60db0cc635ffcb5993372fbd16cdd8ce4bb7a
yuuma-0212 <yuumaskate_yk@yahoo.co.jp>
2023-05-25 16:33:18 +0900

    nuxt.config.jsのbaseURLを変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -76,7 +76,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     //baseURL: process.env.BROWSER_BASE_URL,
-    baseURL: "http://localhost:3000"
+    baseURL: process.env.BASE_URL
   },
 
   /*
